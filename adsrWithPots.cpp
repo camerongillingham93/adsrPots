f063e7cafc45ee5ebc0cb748c86f031ce9e27305
CameronG <cameron.gillingham1@gmail.com>
2024-01-03 16:30:22 -0800

    pots 0 and 1 working

--- adsrWithPots.cpp
+++ adsrWithPots.cpp
@@ -1,28 +1,100 @@
+// adsr envelope example
+
 #include "daisysp.h"
 #include "daisy_seed.h"
 
+// Shortening long macro for sample rate
+#ifndef sample_rate
 
-using namespace daisy;
+#endif
+
+// Interleaved audio definitions
+#define LEFT (i)
+#define RIGHT (i + 1)
+
 using namespace daisysp;
+using namespace daisy;
 
-DaisySeed hw;
+static DaisySeed  hw;
+static Adsr       env;
+static Oscillator osc;
+static Metro      tick;
+bool              gate;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
+                          AudioHandle::InterleavingOutputBuffer out,
+                          size_t                                size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    float osc_out, env_out;
+    for(size_t i = 0; i < size; i += 2)
+    {
+        // When the metro ticks, trigger the envelope to start.
+        if(tick.Process())
+        {
+            gate = !gate;
+        }
+
+        // Use envelope to control the amplitude of the oscillator.
+        env_out = env.Process(gate);
+        osc.SetAmp(env_out);
+        osc_out = osc.Process();
+
+        out[LEFT]  = osc_out;
+        out[RIGHT] = osc_out;
+    }
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
-	hw.StartAudio(AudioCallback);
-	while(1) {int x = 10;}
-	
+    // initialize seed hardware and daisysp modules
+    float sample_rate;
+    hw.Configure();
+    hw.Init();
+    hw.SetAudioBlockSize(4);
+    sample_rate = hw.AudioSampleRate();
+    env.Init(sample_rate);
+    osc.Init(sample_rate);
 
+    // set up ADC's
+    AdcChannelConfig adc_cfg[3];
+    adc_cfg[0].InitSingle(seed::A0);
+    adc_cfg[1].InitSingle(seed::A1);
+    adc_cfg[2].InitSingle(seed::A2);
+    adc_cfg[2].InitSingle(seed::A3);
+
+    // Initialize the ADC peripheral with that configuration
+    hw.adc.Init(adc_cfg, 4);
+
+    /** Start the ADC conversions in the background */
+    hw.adc.Start();
+
+    // Set up metro to pulse every second
+    tick.Init(1.0f, sample_rate);
+
+    //Set envelope parameters
+    env.SetTime(ADSR_SEG_ATTACK, 1);
+    env.SetTime(ADSR_SEG_DECAY, .1);
+    env.SetTime(ADSR_SEG_RELEASE, .01);
+
+    env.SetSustainLevel(.25);
+
+    // Set parameters for oscillator
+    osc.SetWaveform(osc.WAVE_TRI);
+    osc.SetFreq(220);
+    osc.SetAmp(0.25);
+
+    // Start logging for printing over serial
+    hw.StartLog(); 
+    
+    // start callback
+    hw.StartAudio(AudioCallback);
+
+    while(1) 
+    {
+        hw.PrintLine("input 1: %d",hw.adc.Get(0));
+        hw.PrintLine("input 2: %d",hw.adc.Get(1));
+        hw.PrintLine("input 3: %d",hw.adc.Get(2));
+        hw.PrintLine("input 4: %d",hw.adc.Get(3));
+        hw.DelayMs(250);
+    }
 }
